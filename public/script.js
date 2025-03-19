ab3ea56b2079a0a157af570830b5ad1134e0973b
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2025-03-19 13:52:42 -0300

    adicionada função calcularDias em script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
+function calcularDias() {
+    const dataInput = document.getElementById("dataAcampamento").value;
+    
+    if (!dataInput) {
+        alert("Por favor, insira uma data válida.");
+        return;
+    }
+    
+    const dataAcampamento = new Date(dataInput);
+    const hoje = new Date();
+    const diferenca = Math.floor((hoje - dataAcampamento) / (1000 * 60 * 60 * 24));
 
+    document.getElementById("resultado").innerText = `Se passaram ${diferenca} dias desde sua vida nova.`;
+}
